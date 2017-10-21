05aa905c3905708e051e699ceeca595edcd025f3
iharrymartin <harrymartin@live.com>
2017-10-21 23:42:41 +0530

    Update 8.10.py

--- 8.10.py
+++ 8.10.py
@@ -1,11 +1,7 @@
 import sys
-def string(x):
- index = -1
- for f in x:
-  print x[index]
-  index = index - 1
-print "Enter your string"
 a = raw_input()
-string(a):
-if __name__ == "__string__":
-string(a)
+b = a[::-1]
+if b == a:
+ print("is_pallandrom)
+else:
+ print 'not pallandrome'
