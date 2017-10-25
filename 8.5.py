633475f78131701a0ad6052e89b778d9f92f79b6
iharrymartin <harrymartin@live.com>
2017-10-25 22:28:51 +0530

    update

---
+++ 8.5.py
@@ -0,0 +1,13 @@
+import sys
+def count(x):
+ count = 0
+ for letter in x:
+   if letter == 'a':
+ 		count = count + 1
+ print count
+print 'enter the word'
+a = raw_input()
+print "enter the aplhabet to be counted"
+count(a)
+if __name__ =="'__count__":
+ count(a)
