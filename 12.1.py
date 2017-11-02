5f666a99c36ee5ce622a3e9cf86f1275d01e5d81
iharrymartin <harrymartin@live.com>
2017-11-02 08:51:47 -0700

    update

---
+++ 12.1.py
@@ -0,0 +1,12 @@
+import sys
+def sumall(x):
+ c = sum(x)
+ print c
+a= int(input("enter limit"))
+b =[0 for i in range(a)]
+for i in range(a):
+ b[i]=int(input("enter num\t:"))
+f=tuple(b)
+sumall(f)
+if __name__ == "sumall":
+ sumall(f)
